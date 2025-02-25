17157acaad942c674a0c77a0a85b1045ba173273
maria-lux <maria.amcl@gmail.com>
2025-02-25 08:58:15 +0000

    Initial commit aval3

--- week3/ResizingArrayQueueOfStrings.java
+++ week3/ResizingArrayQueueOfStrings.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ResizingArrayQueueOfStrings {
+public class ResizingArrayQueueOfStrings {
+
+    private String[] s;
+    private int N;
+
+    public ResizingArrayQueueOfStrings() {
+        s = new String[N];
+    }
+
+    public void enqueue(String item) {
+
+    }
+
+    public String dequeue() {
+        return "";
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void shift() {
+    }
+
 }
