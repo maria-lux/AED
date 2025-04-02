427e6aee4af186023ca1ebde83dae764aad73602
maria-lux <maria.amcl@gmail.com>
2025-04-02 15:57:28 +0100

    initial commit for week 7, quick sort

---
+++ week7/QuickSorting.java
@@ -0,0 +1,41 @@
+public class QuickSorting {
+
+    /** sorts the array a using the quicksort algorithm */
+    public static void sort(Comparable[] a) {
+
+    }
+
+    /** (recursively) sorts the sub-array a[lo] to a[hi] */
+    public static void sortSubarray(Comparable[] a, int lo, int hi) {
+
+    }
+
+    /** performs insertion sorting on the sub-array a[lo] to a[hi] */
+    public static void insertionSort(Comparable[] a, int lo, int hi) {
+
+    }
+    /** partition the sub-array a[lo] to a[hi] */
+    public static int partition(Comparable[] a, int lo, int hi) {
+        return 0;
+    }
+
+    /** returns the index of the median of 3 random elements in the sub-array a[lo] to a[hi] */
+    public static int medianOfThree(Comparable[] a, int lo, int hi) {
+        return 0;
+    }
+    /** is a <= b? */
+    public static boolean lessOrEqual(Comparable a, Comparable b) {
+        return false;
+    }
+
+    /** swaps a[i] and a[j] */
+    public static void exchange(Comparable[] a, int i, int j) {
+
+    }
+
+    /** shuffles a using the Knuth shuffle algorithm */
+    public static void shuffle(Comparable[] a) {
+
+    }
+
+}
